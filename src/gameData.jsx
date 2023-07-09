7dd21b967b2d595a65b8c2192cdce09e574a2c88
spycewyrm <ytx0a58kxcog@opayq.com>
2023-07-09 00:21:39 -0400

    Prettified code

--- src/gameData.jsx
+++ src/gameData.jsx
@@ -1,104 +1,144 @@
 let gameData = [
-    {
-        title: "Alien: Isolation",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/214490/header.jpg?t=1668768027",
-        blurb: "Discover the true meaning of fear in Alien: Isolation, a survival horror set in an atmosphere of constant dread and mortal danger."
-    },
-    {
-        title: "Aliens: Dark Descent",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1150440/header.jpg?t=1687277023",
-        blurb: "Drop into the gripping journey of Aliens: Dark Descent, a squad-based, single-player action game in the iconic Alien franchise. Lead your soldiers in real-time to stop a new and terrifying kind of Xenomorph outbreak on Planet Lethe.",
-    },
-    {
-        title: "Bramble: The Mountain King",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1623940/header.jpg?t=1687179668",
-        blurb: "Bramble The Mountain King is a grim adventure set in a world inspired by dark, Nordic fables. Explore the beautiful yet dangerous and twisted land of Bramble in your endeavour to rescue your sister. Traverse a wondrous landscape and survive deadly encounters with Bramble's many hideous creatures.",
-    },
-    {
-        title: "Cyberpunk 2077",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header.jpg?t=1687178759",
-        blurb: "Cyberpunk 2077 is an open-world, action-adventure RPG set in the dark future of Night City — a dangerous megalopolis obsessed with power, glamor, and ceaseless body modification."
-    },
-    {
-        title: "Control Ultimate Edition",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/870780/header.jpg?t=1687435620",
-        blurb: "Winner of over 80 awards, Control is a visually stunning third-person action-adventure that will keep you on the edge of your seat."
-    },
-    {
-        title: "Detroit: Become Human",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1222140/header.jpg?t=1667468479",
-        blurb: "Detroit: Become Human puts the destiny of both mankind and androids in your hands, taking you to a near future where machines have become more intelligent than humans. Every choice you make affects the outcome of the game, with one of the most intricately branching narratives ever created."
-    },
-    {
-        title: "Deus Ex: Mankind Divided",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/337000/header.jpg?t=1675708510",
-        blurb: "Now an experienced covert operative, Adam Jensen is forced to operate in a world that has grown to despise his kind. Armed with a new arsenal of state-of-the-art weapons and augmentations, he must choose the right approach, along with who to trust, in order to unravel a vast worldwide conspiracy."
-    },
-    {
-        title: "Dishonored",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/205100/header.jpg?t=1681845707",
-        blurb: "Dishonored is an immersive first-person action game that casts you as a supernatural assassin driven by revenge. With Dishonored's flexible combat system, creatively eliminate your targets as you combine the supernatural abilities, weapons and unusual gadgets at your disposal."
-    },
-    {
-        title: "Dragon Age™ Inquisition",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1222690/header.jpg?t=1668811031",
-        blurb: "Winner of over 130 Game of the Year awards, discover the definitive Dragon Age: Inquisition experience. The Game of the Year Edition includes the critically acclaimed game, all three official add-ons - Jaws of Hakkon, The Descent, and Trespasser - and more."
-    },
-    {
-        title: "Gamedec",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/917720/header.jpg?t=1686038690",
-        blurb: "Gamedec is a single-player cyberpunk isometric RPG. You are a game detective, who solves crimes inside virtual worlds. Use your wits to gather info from your witnesses and suspects, getting to the bottom of deceptive schemes. The game continually adapts to your decisions and never judges."
-    },
-    {
-        title: "Ghost of a Tale",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/417290/header.jpg?t=1667033370",
-        blurb: "Ghost of a Tale is an action-RPG game in which you play as Tilo, a mouse and minstrel thrown into a perilous adventure. Through stealth and cunning you'll be able to explore the secrets of Dwindling Heights Keep and navigate its dangers."
-    },
-    {
-        title: "GreedFall",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/606880/header.jpg?t=1663150674",
-        blurb: "Engage in a core roleplaying experience, and forge the destiny of a new world seeping with magic, and filled with riches, lost secrets, and fantastic creatures. With diplomacy, deception and force, become part of a living, evolving world - influence its course and shape your story."
-    },
-    {
-        title: "Hellblade: Senua's Sacrifice",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/414340/header.jpg?t=1661444431",
-        blurb: "From the makers of Heavenly Sword, Enslaved: Odyssey to the West, and DmC: Devil May Cry, comes a warrior's brutal journey into myth and madness. Set in the Viking age, a broken Celtic warrior embarks on a haunting vision quest into Viking Hell to fight for the soul of her dead lover."
-    },
-    {
-        title: "Kena: Bridge of Spirits",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1954200/header.jpg?t=1664298117",
-        blurb: "A story-driven, action adventure combining exploration with fast-paced combat. Untangle the past as Kena, a young Spirit Guide in search of the sacred Mountain Shrine. Help free the spirits trapped in a forgotten village with the help of the Rot, her adorable (yet powerful) spirit companions."
-    },
-    {
-        title: "Mass Effect™ Legendary Edition",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1328670/header.jpg?t=1669773470",
-        blurb: "The Mass Effect™ Legendary Edition includes single-player base content and over 40 DLC from the highly acclaimed Mass Effect, Mass Effect 2, and Mass Effect 3 games, including promo weapons, armors, and packs — remastered and optimized for 4K Ultra HD."
-    },
-    {
-        title: "Ori and the Blind Forest",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/261570/header.jpg?t=1667504148",
-        blurb: "'Ori and the Blind Forest' tells the tale of a young orphan destined for heroics, through a visually stunning action-platformer crafted by Moon Studios for PC."
-    },
-    {
-        title: "Pentiment",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1205520/header.jpg?t=1685035358",
-        blurb: "Step into a living illustrated world in a time when Europe is at a crossroads of great religious and political change. Walk in the footsteps of Andreas Maler, a master artist who finds himself in the middle of murders, scandals, and intrigue in the Bavarian Alps."
-    },
-    {
-        title: "Remember Me",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/228300/header.jpg?t=1644283394",
-        blurb: "Remember Me™ is a 3rd person action adventure where players take on the role of Nilin, a former elite memory hunter with the ability to break into people's minds and steal or even alter their memories."
-    },
-    {
-        title: "Solasta: Crown of the Magister",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1096530/header.jpg?t=1685037606",
-        blurb: "Roll for initiative, take attacks of opportunity, manage player location and the verticality of the battle field in this Turn-Based Tactical RPG based on the SRD 5.1 Ruleset. In Solasta, you make the choices, dice decide your destiny."
-    },
-    {
-        title: "Vampyr",
-        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/427290/header.jpg?t=1651049845",
-        blurb: "London, 1918. You are newly-turned Vampyr Dr. Jonathan Reid. As a doctor, you must find a cure to save the city's flu-ravaged citizens. As a Vampyr, you are cursed to feed on those you vowed to heal."
-    }
+  {
+    title: "Alien: Isolation",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/214490/header.jpg?t=1668768027",
+    blurb:
+      "Discover the true meaning of fear in Alien: Isolation, a survival horror set in an atmosphere of constant dread and mortal danger.",
+  },
+  {
+    title: "Aliens: Dark Descent",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1150440/header.jpg?t=1687277023",
+    blurb:
+      "Drop into the gripping journey of Aliens: Dark Descent, a squad-based, single-player action game in the iconic Alien franchise. Lead your soldiers in real-time to stop a new and terrifying kind of Xenomorph outbreak on Planet Lethe.",
+  },
+  {
+    title: "Bramble: The Mountain King",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1623940/header.jpg?t=1687179668",
+    blurb:
+      "Bramble The Mountain King is a grim adventure set in a world inspired by dark, Nordic fables. Explore the beautiful yet dangerous and twisted land of Bramble in your endeavour to rescue your sister. Traverse a wondrous landscape and survive deadly encounters with Bramble's many hideous creatures.",
+  },
+  {
+    title: "Cyberpunk 2077",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header.jpg?t=1687178759",
+    blurb:
+      "Cyberpunk 2077 is an open-world, action-adventure RPG set in the dark future of Night City — a dangerous megalopolis obsessed with power, glamor, and ceaseless body modification.",
+  },
+  {
+    title: "Control Ultimate Edition",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/870780/header.jpg?t=1687435620",
+    blurb:
+      "Winner of over 80 awards, Control is a visually stunning third-person action-adventure that will keep you on the edge of your seat.",
+  },
+  {
+    title: "Detroit: Become Human",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1222140/header.jpg?t=1667468479",
+    blurb:
+      "Detroit: Become Human puts the destiny of both mankind and androids in your hands, taking you to a near future where machines have become more intelligent than humans. Every choice you make affects the outcome of the game, with one of the most intricately branching narratives ever created.",
+  },
+  {
+    title: "Deus Ex: Mankind Divided",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/337000/header.jpg?t=1675708510",
+    blurb:
+      "Now an experienced covert operative, Adam Jensen is forced to operate in a world that has grown to despise his kind. Armed with a new arsenal of state-of-the-art weapons and augmentations, he must choose the right approach, along with who to trust, in order to unravel a vast worldwide conspiracy.",
+  },
+  {
+    title: "Dishonored",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/205100/header.jpg?t=1681845707",
+    blurb:
+      "Dishonored is an immersive first-person action game that casts you as a supernatural assassin driven by revenge. With Dishonored's flexible combat system, creatively eliminate your targets as you combine the supernatural abilities, weapons and unusual gadgets at your disposal.",
+  },
+  {
+    title: "Dragon Age™ Inquisition",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1222690/header.jpg?t=1668811031",
+    blurb:
+      "Winner of over 130 Game of the Year awards, discover the definitive Dragon Age: Inquisition experience. The Game of the Year Edition includes the critically acclaimed game, all three official add-ons - Jaws of Hakkon, The Descent, and Trespasser - and more.",
+  },
+  {
+    title: "Gamedec",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/917720/header.jpg?t=1686038690",
+    blurb:
+      "Gamedec is a single-player cyberpunk isometric RPG. You are a game detective, who solves crimes inside virtual worlds. Use your wits to gather info from your witnesses and suspects, getting to the bottom of deceptive schemes. The game continually adapts to your decisions and never judges.",
+  },
+  {
+    title: "Ghost of a Tale",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/417290/header.jpg?t=1667033370",
+    blurb:
+      "Ghost of a Tale is an action-RPG game in which you play as Tilo, a mouse and minstrel thrown into a perilous adventure. Through stealth and cunning you'll be able to explore the secrets of Dwindling Heights Keep and navigate its dangers.",
+  },
+  {
+    title: "GreedFall",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/606880/header.jpg?t=1663150674",
+    blurb:
+      "Engage in a core roleplaying experience, and forge the destiny of a new world seeping with magic, and filled with riches, lost secrets, and fantastic creatures. With diplomacy, deception and force, become part of a living, evolving world - influence its course and shape your story.",
+  },
+  {
+    title: "Hellblade: Senua's Sacrifice",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/414340/header.jpg?t=1661444431",
+    blurb:
+      "From the makers of Heavenly Sword, Enslaved: Odyssey to the West, and DmC: Devil May Cry, comes a warrior's brutal journey into myth and madness. Set in the Viking age, a broken Celtic warrior embarks on a haunting vision quest into Viking Hell to fight for the soul of her dead lover.",
+  },
+  {
+    title: "Kena: Bridge of Spirits",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1954200/header.jpg?t=1664298117",
+    blurb:
+      "A story-driven, action adventure combining exploration with fast-paced combat. Untangle the past as Kena, a young Spirit Guide in search of the sacred Mountain Shrine. Help free the spirits trapped in a forgotten village with the help of the Rot, her adorable (yet powerful) spirit companions.",
+  },
+  {
+    title: "Mass Effect™ Legendary Edition",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1328670/header.jpg?t=1669773470",
+    blurb:
+      "The Mass Effect™ Legendary Edition includes single-player base content and over 40 DLC from the highly acclaimed Mass Effect, Mass Effect 2, and Mass Effect 3 games, including promo weapons, armors, and packs — remastered and optimized for 4K Ultra HD.",
+  },
+  {
+    title: "Ori and the Blind Forest",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/261570/header.jpg?t=1667504148",
+    blurb:
+      "'Ori and the Blind Forest' tells the tale of a young orphan destined for heroics, through a visually stunning action-platformer crafted by Moon Studios for PC.",
+  },
+  {
+    title: "Pentiment",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1205520/header.jpg?t=1685035358",
+    blurb:
+      "Step into a living illustrated world in a time when Europe is at a crossroads of great religious and political change. Walk in the footsteps of Andreas Maler, a master artist who finds himself in the middle of murders, scandals, and intrigue in the Bavarian Alps.",
+  },
+  {
+    title: "Remember Me",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/228300/header.jpg?t=1644283394",
+    blurb:
+      "Remember Me™ is a 3rd person action adventure where players take on the role of Nilin, a former elite memory hunter with the ability to break into people's minds and steal or even alter their memories.",
+  },
+  {
+    title: "Solasta: Crown of the Magister",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1096530/header.jpg?t=1685037606",
+    blurb:
+      "Roll for initiative, take attacks of opportunity, manage player location and the verticality of the battle field in this Turn-Based Tactical RPG based on the SRD 5.1 Ruleset. In Solasta, you make the choices, dice decide your destiny.",
+  },
+  {
+    title: "Vampyr",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/427290/header.jpg?t=1651049845",
+    blurb:
+      "London, 1918. You are newly-turned Vampyr Dr. Jonathan Reid. As a doctor, you must find a cure to save the city's flu-ravaged citizens. As a Vampyr, you are cursed to feed on those you vowed to heal.",
+  },
 ];
 
 export default gameData;
--- src/makeFilter.jsx
+++ src/makeFilter.jsx
@@ -1,10 +1,10 @@
-function GenreFilter({genre}) {
-    return (
-        <div className="genre">
-            <img className="genre-icon" src={genre.icon}></img>
-            <div className="genre-name">{genre.name}</div>
-        </div>
-    );
+function GenreFilter({ genre }) {
+  return (
+    <div className="genre">
+      <img className="genre-icon" src={genre.icon}></img>
+      <div className="genre-name">{genre.name}</div>
+    </div>
+  );
 }
 
 export default GenreFilter;
